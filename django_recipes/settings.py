374acc4741a6f1ef55dfdf002dd3509bc2b63384
homosociologicus <michaelbaytenov@gmail.com>
2022-07-02 22:46:34 +0300

    added allowed host

--- django_recipes/settings.py
+++ django_recipes/settings.py
@@ -26,9 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_RECIPES_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get("DJANGO_DEBUG")))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "mikebayt-django-recipes.herokuapp.com",
+]
 
 
 # Application definition
